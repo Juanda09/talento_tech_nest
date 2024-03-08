35559bb40924664125e0bfe6565921edfd97e915
j58937672 <j58937672@gmail.com>
2024-03-08 18:44:14 -0500

    Agregar validaciones en la creacion de usuarios

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,35 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+} from "class-validator";
+
 export class CreateUserDto {
+  @IsString({ message: "El nombre debe ser una cadena de caracteres." })
+  @Length(2, 50, { message: "El nombre debe tener entre 2 y 50 caracteres." })
   readonly name: string;
+
+  @IsEmail({}, { message: "El email proporcionado no es válido." })
   readonly email: string;
+
+  @IsString({ message: "La contraseña debe ser una cadena de caracteres." })
+  @IsNotEmpty({ message: "La contraseña no puede estar vacía." })
+  @Length(6, 20, {
+    message: "La contraseña debe tener entre 6 y 20 caracteres.",
+  })
+  @Matches(/^(?=.*[A-Z])(?=.*[!@#$%^&*]).{6,20}$/, {
+    message:
+      "La contraseña debe contener al menos una letra mayúscula y un carácter especial.",
+  })
   readonly password: string;
-  readonly Avatar: string;
+
+  readonly avatar: string;
+
+  @IsString({ message: "El apellido debe ser una cadena de caracteres." })
+  @Length(2, 50, { message: "El apellido debe tener entre 2 y 50 caracteres." })
   readonly lastname: string;
+
   readonly age: number;
 }
