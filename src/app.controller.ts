550676d581ae28a2ed595db229de4778b12eb071
j58937672 <j58937672@gmail.com>
2024-03-07 20:44:05 -0500

    Modificacion app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,7 @@
-import { Controller, Get, Param, Post } from "@nestjs/common";
+import { Controller } from "@nestjs/common";
 import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get("hello/:name")
-  getHello(@Param("name") name: string): string {
-    return this.appService.getHello(name);
-  }
-  @Get("goodbye/:name")
-  sayGoodbye(@Param("name") name: string): string {
-    return this.appService.sayGoodbye(name);
-  }
-  @Post("hello/:name")
-  postHello(@Param("name") name: string): string {
-    return this.appService.getHello(name);
-  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,4 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class AppService {
-  getHello(name): string {
-    return "Hola " + name;
-  }
-  sayGoodbye(name): string {
-    return "Adios " + name;
-  }
-}
+export class AppService {}
